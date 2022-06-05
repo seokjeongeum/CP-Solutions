2f008faa8478c21a93e6239b195d9d5b9a7cfe7f
석정음 Seok, Jeongeum <33026380+seokjeongeum@users.noreply.github.com>
2022-06-06 07:55:27 +0900

    Committing files

---
+++ Atcoder/C++ (GCC 9.2.1)/abc196_a/21061078.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <algorithm>
+#include<vector>
+using namespace std;
+int main() {
+	cin.sync_with_stdio(false);
+	cin.tie(nullptr);
+	cout.tie(nullptr);
+	int a, b;
+	cin >> a >> b;
+	int c, d;
+	cin >> c >> d;
+	cout << b - c;
+	return 0;
+}
