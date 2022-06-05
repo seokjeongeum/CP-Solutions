3365b0003548c319ebf35e4025417519cade52a3
석정음 Seok, Jeongeum <33026380+seokjeongeum@users.noreply.github.com>
2022-06-06 07:54:43 +0900

    Committing files

---
+++ Atcoder/C++ (GCC 9.2.1)/abc217_a/25558103.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <queue>
+#include <tuple>
+#include <limits>
+#include <unordered_map>
+#include <stack>
+#include <set>
+#include <unordered_map>
+#include <deque>
+#include <algorithm>
+#include <string>
+#include <vector>
+#include <algorithm>
+using namespace std;
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(nullptr);
+	cout.tie(nullptr);
+	cout.precision(numeric_limits<double>::max_digits10);
+	string S, T;
+	cin >> S >> T;
+	if (S < T) {
+		cout << "Yes" << endl;
+	}
+	else {
+		cout << "No" << endl;
+	}
+	return 0;
+}
